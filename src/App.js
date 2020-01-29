38b23cfd9dc5f0dae386bd61ae5d2167b985b827
Mohit <mohit@pop-os.localdomain>
2020-01-29 12:44:19 +0530

    basic routing done in the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path = '/' component = {Homepage} />
     </div>
   );
 }
